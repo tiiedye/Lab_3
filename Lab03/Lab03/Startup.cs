0ee2333c509978f5e5384c9612c8ee2c935f4d08
tiiedye <tiiedye@gmail.com>
2020-10-28 11:16:36 -0700

    creating project files

---
+++ Lab03/Lab03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab03.Startup))]
+namespace Lab03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
